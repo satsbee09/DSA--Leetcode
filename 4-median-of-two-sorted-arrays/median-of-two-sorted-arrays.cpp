22030d8d0e0762023c6c35a0432ed682d4d4bce6
satsbee <168117408+satsbee09@users.noreply.github.com>
2026-04-27 23:49:53 +0530

    Time: 3 ms (28.85%) | Memory: 96 MB (9.40%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if (nums1.size() > nums2.size()) {
-            return findMedianSortedArrays(nums2, nums1);
+        vector<int>ans;
+        for(int i=0;i<nums1.size();i++){
+            ans.push_back(nums1[i]);
+        }
+        for(int i=0;i<nums2.size();i++){
+            ans.push_back(nums2[i]);
+        }
+        sort(ans.begin(),ans.end());
+        int n=ans.size();
+        double val=0;
+        if(n%2==1){
+             val=ans[n/2];
+        }
+        else{
+            val =(ans[n/2]+ans[(n/2)-1])/2.0;
         }
 
-        int len1 = nums1.size(), len2 = nums2.size();
-        int left = 0, right = len1;
-
-        while (left <= right) {
-            int part1 = (left + right) / 2;
-            int part2 = (len1 + len2 + 1) / 2 - part1;
-
-            int maxLeft1 = (part1 == 0) ? INT_MIN : nums1[part1 - 1];
-            int minRight1 = (part1 == len1) ? INT_MAX : nums1[part1];
-            int maxLeft2 = (part2 == 0) ? INT_MIN : nums2[part2 - 1];
-            int minRight2 = (part2 == len2) ? INT_MAX : nums2[part2];
-
-            if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
-                if ((len1 + len2) % 2 == 0) {
-                    return (max(maxLeft1, maxLeft2) + min(minRight1, minRight2)) / 2.0;
-                } else {
-                    return max(maxLeft1, maxLeft2);
-                }
-            } else if (maxLeft1 > minRight2) {
-                right = part1 - 1;
-            } else {
-                left = part1 + 1;
-            }
-        }
-
-        return 0.0;        
+        return  val;
+        
     }
 };
