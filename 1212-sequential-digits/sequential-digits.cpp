6a36efc8ae8211d743c9db9922bff39592801242
satsbee <168117408+satsbee09@users.noreply.github.com>
2026-07-13 23:41:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (76.91%) - LeetSync

---
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        vector<int>ans;
+        string s="123456789";
+        string l=to_string(low);
+        string k=to_string(high);
+        
+        for(int i=l.size();i<=k.size();i++){
+            for(int j=0;j<=9-i;j++){
+               string n=s.substr(j,i);
+               int num=stoi(n);
+               if(num>=low&&num<=high){
+                ans.push_back(num);
+               } 
+            }
+        }
+        return ans;
+        
+
+        
+    }
+};
