eca3527e25d1fdd30f3bcc93bf8e0aa14d1a40ef
satsbee <168117408+satsbee09@users.noreply.github.com>
2026-07-27 12:35:05 +0530

    Time: 724 ms (75.65%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -6,5 +6,5 @@ FROM Products p
 JOIN Orders o
 ON p.product_id = o.product_id
 WHERE o.order_date BETWEEN '2020-02-01' AND '2020-02-29'
-GROUP BY p.product_id, p.product_name
+GROUP BY p.product_id
 HAVING SUM(o.unit) >= 100;
