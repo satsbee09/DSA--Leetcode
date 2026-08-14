9d82c9578820096126a3752ba2c656d1704dfe5b
satsbee <168117408+satsbee09@users.noreply.github.com>
2026-08-15 01:10:19 +0530

    Time: 7 ms (16.83%) | Memory: 9.6 MB (44.92%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int maximumLengthSubstring(string s) {
-       
         int n=s.size();
         int ans=INT_MIN;
+        int j=0;
+        unordered_map<char,int>mp;
         for(int i=0;i<n;i++){
-             int arr[26]={0};
-            for(int j=i;j<n;j++){
-                arr[s[j]-'a']++;
-                if(arr[s[j]-'a']>2){
-                    break;
-                }
-                ans=max(ans,j-i+1);
-            
-        }
+          mp[s[i]]++;
+          while(mp[s[i]]>2){
+            mp[s[j]]--;
+            if(mp[s[j]]==0){
+                mp.erase(s[j]);
+            }
+            j++;
+          }
+          ans=max(ans,i-j+1);
+        
         }
         return ans;
-        
         
     }
 };
