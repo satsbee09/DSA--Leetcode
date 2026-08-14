79332f1afca1e73f4dbcb5d409a2a096124ce932
satsbee <168117408+satsbee09@users.noreply.github.com>
2026-08-15 01:50:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (71.75%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+       double solve(double x, long long n){
+        if(n==0)return 1;
+        double half=solve(x,n/2);
+        if(n%2==0)
+        return half*half;
+
+        return x*half*half;
+       }
+    double myPow(double x, int n) {
+        long long N=n;
+        if(n<0){
+           x=1/x;
+           N=-N;
+        
+        }
+        return  solve(x,n);
+        
+    }
+};
