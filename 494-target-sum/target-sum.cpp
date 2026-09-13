abdb72e95c3213a5554b9e22977a18ddd0f042dd
satsbee <168117408+satsbee09@users.noreply.github.com>
2026-09-13 14:59:14 +0530

    Time: 15 ms (50.87%) | Memory: 25.9 MB (22.73%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
-    void solve(int i,vector<int>& nums,int target,int &count){
+    int solve(int i,vector<int>& nums,int currsum,int target, int sum ,vector<vector<int>>&dp){
         if(i>=nums.size()){
-            if(target==0){
-                count++;
-            }
-            return ;
+            return currsum == target ? 1 : 0;
         }
-        solve(i+1,nums,target+nums[i],count);
-        solve(i+1,nums,target-nums[i],count);
+        
+        int idx=currsum+sum;
+        if(dp[i][idx]!=-1){
+            return dp[i][idx];
+        }
+
+        int add=solve(i+1,nums,currsum+nums[i],target,sum,dp);
+         int subtract=solve(i+1,nums,currsum-nums[i],target,sum,dp);
+         return dp[i][idx]=add+subtract;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int count=0;
-        solve(0,nums,target,count);
-        return count;
+        
+        int n=nums.size();
+        int sum=accumulate(nums.begin(),nums.end(),0);
+        vector<vector<int>>dp(n,vector<int>(2*sum+1,-1));
+        if(abs(target)>sum){
+            return 0;
+        }
+        return solve(0,nums,0,target,sum,dp);
+        
     }
 };
